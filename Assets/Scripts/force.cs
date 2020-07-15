20301cf83a99de1fe2a71808c21c570d8268a216
JhonSme <jonasrpg00@gmail.com>
2020-07-15 00:18:31 -0300

    Projetin third
    0.1.1.1.1
    Add:
    Criação do livro (Prefab) (temporario)
    Add imagens de place_holders
    
    Mudanças:
    Alteração na escada (função botão)
    Mudança na troca tela
    
    Scripts:
    Andar basico

---
+++ Assets/Scripts/force.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour{
+    public float Speed;
+    void Start () {
+
+    }
+    
+     // Update is called once per frame
+    void Update () {
+
+    if (Input.GetKey(KeyCode.W)){
+        transform.position += transform.up * Speed * Time.deltaTime;
+        }
+    else if (Input.GetKey(KeyCode.S)){
+        transform.position -= transform.up * Speed * Time.deltaTime;
+        }
+ 
+    if (Input.GetKey(KeyCode.D)){
+        transform.position += transform.right * Speed * Time.deltaTime;
+        }
+    else if (Input.GetKey(KeyCode.A)){
+        transform.position -= transform.right * Speed * Time.deltaTime;
+        }
+    }
+}
