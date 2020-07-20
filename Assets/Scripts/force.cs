443dd209b9f8775dcd11b8fcdd199a425dbe161f
JhonSme <jonasrpg00@gmail.com>
2020-07-20 16:58:34 -0300

    0.1.1.1.1.1.1.0.1
    Ta virando IP já kkk
    
    Add:
    Build
    Config da build (qualidades)
    Force.cs (movimentação do Player[Incompleto])
    Escalonamento com a tela
    Esc sai do jogo
    
    Mudança:
    Mudança no Force (pode ser controlado pelo unity, mais suave)
    Animação do livro
    Alguns sprites
    
    Retirado:

--- Assets/Scripts/force.cs
+++ Assets/Scripts/force.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class force : MonoBehaviour{
     public float Speed;
+    //public CollisionInfo collisions;
+    //private const int LIMITES_LAYER = 12;
+    //public LayerMask collisionMask;
     void Start () {
 
     }
@@ -11,18 +15,22 @@ public class force : MonoBehaviour{
      // Update is called once per frame
     void Update () {
 
-    if (Input.GetKey(KeyCode.W)){
-        transform.position += transform.up * Speed * Time.deltaTime;
+    if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)){
+        transform.position += transform.up * ((float)(Speed/1.3)) * Time.deltaTime;
         }
-    else if (Input.GetKey(KeyCode.S)){
-        transform.position -= transform.up * Speed * Time.deltaTime;
+    else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){
+        transform.position -= transform.up * ((float)(Speed/1.3)) * Time.deltaTime;
         }
  
-    if (Input.GetKey(KeyCode.D)){
+    if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)){
         transform.position += transform.right * Speed * Time.deltaTime;
         }
-    else if (Input.GetKey(KeyCode.A)){
+    else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){
         transform.position -= transform.right * Speed * Time.deltaTime;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("ta indo?");
     }
 }
